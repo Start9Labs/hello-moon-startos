e950da0f1e04c027bae09acaaa309946ed4136a6
Matt Hill <mattnine@protonmail.com>
2025-06-11 20:24:36 -0600

    beta 28

--- startos/main.ts
+++ startos/main.ts
@@ -1,5 +1,4 @@
 import { sdk } from './sdk'
-import { T } from '@start9labs/start-sdk'
 import { manifest as helloWorldManifest } from 'hello-world-startos/startos/manifest'
 import { uiPort } from './utils'
 
@@ -15,51 +14,41 @@ export const main = sdk.setupMain(async ({ effects, started }) => {
   depResult.throwIfNotSatisfied()
 
   /**
-   * ======================== Additional Health Checks (optional) ========================
-   *
-   * In this section, we define *additional* health checks beyond those included with each daemon (below).
-   */
-  const additionalChecks: T.HealthCheck[] = []
-
-  /**
    * ======================== Daemons ========================
    *
    * In this section, we create one or more daemons that define the service runtime.
    *
    * Each daemon defines its own health check, which can optionally be exposed to the user.
    */
-  return sdk.Daemons.of(effects, started, additionalChecks).addDaemon(
-    'primary',
-    {
-      subcontainer: await sdk.SubContainer.of(
-        effects,
-        { imageId: 'hello-moon' },
-        sdk.Mounts.of()
-          .mountVolume({
-            volumeId: 'main',
-            subpath: null,
-            mountpoint: '.data',
-            readonly: false,
-          })
-          .mountDependency<typeof helloWorldManifest>({
-            dependencyId: 'hello-world',
-            volumeId: 'main',
-            subpath: null,
-            mountpoint: '/hello-world',
-            readonly: true,
-          }),
-        'hello-moon-sub',
-      ),
-      exec: { command: ['hello-world'] },
-      ready: {
-        display: 'Web Interface',
-        fn: () =>
-          sdk.healthCheck.checkPortListening(effects, uiPort, {
-            successMessage: 'The web interface is ready',
-            errorMessage: 'The web interface is unreachable',
-          }),
-      },
-      requires: [],
+  return sdk.Daemons.of(effects, started).addDaemon('primary', {
+    subcontainer: await sdk.SubContainer.of(
+      effects,
+      { imageId: 'hello-moon' },
+      sdk.Mounts.of()
+        .mountVolume({
+          volumeId: 'main',
+          subpath: null,
+          mountpoint: '.data',
+          readonly: false,
+        })
+        .mountDependency<typeof helloWorldManifest>({
+          dependencyId: 'hello-world',
+          volumeId: 'main',
+          subpath: null,
+          mountpoint: '/hello-world',
+          readonly: true,
+        }),
+      'hello-moon-sub',
+    ),
+    exec: { command: ['hello-world'] },
+    ready: {
+      display: 'Web Interface',
+      fn: () =>
+        sdk.healthCheck.checkPortListening(effects, uiPort, {
+          successMessage: 'The web interface is ready',
+          errorMessage: 'The web interface is unreachable',
+        }),
     },
-  )
+    requires: [],
+  })
 })
